d0c6ff68c08323167ffa25843f66a757f9b2662d
Bruno Gama <brunoindie@gmail.com>
2021-07-14 12:59:23 -0300

    Projeto de cobrança de títulos parte 1

---
+++ src/main/java/com/kopperto/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.kopperto.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kopperto.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{
+
+}
